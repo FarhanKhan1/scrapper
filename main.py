4e279f0d55560fc3109070c4a7a278587679ad1b
Farhan Khan <enggfarhan36@gmail.com>
2022-07-20 03:37:08 +0500

    Final commit

--- main.py
+++ main.py
@@ -43,48 +43,11 @@ try:
         result_dictionary[url.rsplit('/', 1)[-1]] = list_for_dic
         list_for_dic = []
 
-    # for x in result_dictionary:
-        # print(x, '=> ',result_dictionary[x])
-        # print("\n*******************************************************************\n")
-
     df = pd.DataFrame.from_dict(result_dictionary, orient='index')
     df = df.transpose()
     # df = pd.DataFrame(result_dictionary)
     print(df)
     df.to_csv('scrapped_specialty_icds.csv')
-
-    # print(result_dictionary)
-    # print(len(result_dictionary))
-
-    # print(len(a_set))
-
-    # r = re.get(a_set.pop())
-    # print(r.url)
-
-    # print("**************************************************************************************")
-    # requests_data1 = re.get(r.url, 'html.parser')
-    # soup1 = BS(requests_data1.text, 'html.parser')
-    # chapter_list_class_data = soup1.findAll('div', class_='code')
-
-    # for span in chapter_list_class_data:
-
-    #     code_set.add(span.find('span').text)
-    
-    # last_word = r.url.rsplit('/', 1)[-1]
-    # print(r.url.rsplit('/', 1)[-1])
-    # keys_list.append(r.url.rsplit('/', 1)[-1])
-    # final_dict[r.url.rsplit('/', 1)[-1]] = None    
-
-    # for icd_10 in code_set:
- 
-    #     list_for_dic.append(icd_10)
-
-
-    # final_dict[last_word] = list_for_dic 
-
-    # print(len(final_dict[last_word]))
-
-
 
 except Response.raise_for_status as e:
     print(e)
